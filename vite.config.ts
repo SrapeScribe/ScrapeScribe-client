b38869db84a12c73208c08e055a8b3eaa8a2f573
nktvit <nikita.vitkovsky@icloud.com>
2024-11-05 11:23:42 +0000

    rebase

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
+// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [sveltekit(), wasm(), topLevelAwait()],
 
