8f4ee8553c2e60f6687b1c7d32973b83fd31d851
nktvit <nikita.vitkovsky@icloud.com>
2024-11-05 11:48:37 +0000

    rust implementation + svelte ssr off

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { greet  } from "../wasm/scraping-instructions/pkg/scraping_instructions.js";
+
     let i = 0
 </script>
 
@@ -6,4 +8,6 @@
     <h1>Page {i}</h1>
     <button on:click={() => i += 1}>Increment</button>
     <button on:click={() => i -= 1}>Decrement</button>
+
+    <h2>{greet("Webassembly")}</h2>
 </main>
