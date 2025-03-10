3d3fe485516bec42c4b462545fb1d97a7ee91380
nktvit <nikita.vitkovsky@icloud.com>
2025-03-10 12:47:27 +0000

    routes for pages

---
+++ src/routes/signin/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+    <title>ScrapeScribe - SignIn</title>
+</svelte:head>
+
+singin
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+    <title>ScrapeScribe - SignUp</title>
+</svelte:head>
+
+signup
