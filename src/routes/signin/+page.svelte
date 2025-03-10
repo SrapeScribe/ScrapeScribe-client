5c00942ae557c656e7307751236785ded1c7331d
nktvit <nikita.vitkovsky@icloud.com>
2025-03-10 18:08:17 +0000

    sign in page logic

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,9 +1,23 @@
 <script lang="ts">
+    import '$lib/aws-amplify'
+    import {auth, handleSignIn, handleSignOut} from "$lib/states/auth.svelte"
 
+    let username = $state("");
+    let password = $state("");
 </script>
 
 <svelte:head>
     <title>ScrapeScribe - SignIn</title>
 </svelte:head>
 
-singin
+{#if auth.user}
+    <p>Welcome, {auth.user?.signInDetails?.loginId ?? auth.user?.username}!</p>
+    <p>User id: {auth.user.userId}</p>
+    <button onclick={handleSignOut}>Sign Out</button>
+{:else}
+    <form onsubmit={() => handleSignIn(username, password)}>
+        <input type="text" bind:value={username} placeholder="Username" />
+        <input type="password" bind:value={password} placeholder="Password" />
+        <button type="submit">Sign In</button>
+    </form>
+{/if}
