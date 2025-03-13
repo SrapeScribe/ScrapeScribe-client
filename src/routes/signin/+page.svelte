f9fdd050d6e9d7a3041a9b5f061bf2ce000621d0
Eoghan55555 <S00233339@atu.ie>
2025-03-13 16:53:09 +0000

    basic sign in/up set up

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,9 +1,27 @@
 <script lang="ts">
-
-</script>
-
-<svelte:head>
-    <title>ScrapeScribe - SignIn</title>
-</svelte:head>
-
-singin
+    import { goto } from '$app/navigation';
+    const switchToSignUp = () => {
+      goto('/signup');
+    };
+    const handleSubmit = (event: SubmitEvent) => {
+      event.preventDefault();
+      const { username, password } = event.target as HTMLFormElement;
+      console.log('Sign In:', (username as HTMLInputElement).value, (password as HTMLInputElement).value);
+    };
+  </script>
+  <style>
+    @import '../../styles/basicstyles.css';
+  </style>
+  <div class="auth-container">
+    <h2>Sign In</h2>
+    <form on:submit={handleSubmit}>
+      <input type="text" placeholder="Username" name="username" required />
+      <input type="password" placeholder="Password" name="password" required />
+      <button type="submit">Sign In</button>
+    </form>
+    <div class="switch">
+      <p>Don't have an account?</p>
+      <a href="javascript:void(0)" on:click={switchToSignUp}>Sign Up</a>
+    </div>
+  </div>
+  
