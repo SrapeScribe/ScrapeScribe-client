0c69893ee255bdf361677244df30aed004c0ed07
Eoghan55555 <S00233339@atu.ie>
2025-03-14 13:54:44 +0000

    sign in page slightly designed

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -1,45 +1,48 @@
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    import '$lib/aws-amplify';
-    import { auth, handleSignIn, handleSignOut } from "$lib/states/auth.svelte";
-    
-    let username = $state("");
-    let password = $state("");
-    
-    const switchToSignUp = () => {
-      goto('/signup');
-    };
-  
-    const handleSubmit = (event: Event) => {
-      event.preventDefault();
-      handleSignIn(username, password);
-    };
-  </script>
-  
-  <svelte:head>
-    <title>ScrapeScribe - SignIn</title>
-  </svelte:head>
-  
-  <style>
-    @import '../../styles/basicstyles.css';
-  </style>
-  
-  <div class="auth-container">
-    {#if auth.user}
-      <p>Welcome, {auth.user?.signInDetails?.loginId ?? auth.user?.username}!</p>
-      <p>User id: {auth.user.userId}</p>
-      <button onclick={handleSignOut}>Sign Out</button>
-    {:else}
-      <h2>Sign In</h2>
-      <form onsubmit={handleSubmit}>
-        <input type="text" bind:value={username} placeholder="Username" required />
-        <input type="password" bind:value={password} placeholder="Password" required />
-        <button type="submit">Sign In</button>
+  import '$lib/aws-amplify'
+  import {auth, handleSignIn, handleSignOut} from "$lib/states/auth.svelte"
+  let username = $state("");
+  let password = $state("");
+</script>
+<svelte:head>
+  <title>ScrapeScribe - SignIn</title>
+</svelte:head>
+{#if auth.user}
+  <p>Welcome, {auth.user?.signInDetails?.loginId ?? auth.user?.username}!</p>
+  <p>User id: {auth.user.userId}</p>
+  <button onclick={handleSignOut}>Sign Out</button>
+{:else}
+<div>
+  <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+    <div class="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
+      <form onsubmit={() => handleSignIn(username, password)} class="mb-0 space-y-6">
+        <div>
+          <label for="Username" class="block text-m font-medium text-gray-700">
+            Username
+          </label>
+        </div>
+        <div class="mt-1">
+        <input 
+            type="text" bind:value={username} placeholder="Username" 
+            class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm
+            focus:outline-none focus:border-black focus:ring-1 focus:ring-black"/>
+        </div>
+        <label for="Password" class="block text-m font-medium text-gray-700">
+          Password
+        </label>
+        <div class="mt-1">
+          <input type="password" bind:value={password} placeholder="Password" 
+          class="w-full border border-gray-300 px-3 py-2 rounded-lg shadow-sm
+          focus:outline-none focus:border-black focus:ring-1 focus:ring-black"/>
+        </div>
+        <div>
+          <button type="submit" class="w-full flex justify-center py-2 px-4
+          border border-transparent rounded-md shadow-sm bg-gray-700 text-white focus:outline-none">
+          Sign In
+        </button>
+        </div>
       </form>
-      <div class="switch">
-        <p>Don't have an account?</p>
-        <a href="javascript:void(0)" onclick={switchToSignUp}>Sign Up</a>
-      </div>
-    {/if}
+    </div>
   </div>
-  
+</div>
+{/if}
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -1,32 +1,8 @@
+
 <script lang="ts">
-    import { goto } from '$app/navigation';
-    const switchToSignIn = () => {
-      goto('/signin');
-    };
-    const handleSubmit = (event: SubmitEvent) => {
-      event.preventDefault();
-      const { username, password, confirmPassword } = event.target as HTMLFormElement;
-      if ((password as HTMLInputElement).value !== (confirmPassword as HTMLInputElement).value) {
-        alert('Passwords do not match!');
-        return;
-      }
-      console.log('Sign Up:', (username as HTMLInputElement).value, (password as HTMLInputElement).value);
-    };
-  </script>
-  <style>
-    @import '../../styles/basicstyles.css';
-  </style>
-  <div class="auth-container">
-    <h2>Sign Up</h2>
-    <form on:submit={handleSubmit}>
-      <input type="text" placeholder="Username" name="username" required />
-      <input type="password" placeholder="Password" name="password" required />
-      <input type="password" placeholder="Confirm Password" name="confirmPassword" required />
-      <button type="submit">Sign Up</button>
-    </form>
-    <div class="switch">
-      <p>Already have an account?</p>
-      <a href="javascript:void(0)" on:click={switchToSignIn}>Sign In</a>
-    </div>
-  </div>
+</script>
+<svelte:head>
+    <title>ScrapeScribe - SignUp</title>
+</svelte:head>
+signup
   
